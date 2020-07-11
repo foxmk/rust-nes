68db4bae3cc610bbb4dfe2f7a939cc03287cbc33
Artem Artemyev <artem.artemyev@springernature.com>
2020-07-11 23:24:26 +0200

    Add much more opcodes

--- src/cpu/mod.rs
+++ src/cpu/mod.rs
@@ -11,6 +11,8 @@ type Memory = dyn IndexMut<Addr, Output=u8>;
 enum Flag {
     N = 0b10000000,
     V = 0b01000000,
+    U = 0b00100000,
+    B = 0b00010000,
     D = 0b00001000,
     I = 0b00000100,
     Z = 0b00000010,
@@ -46,11 +48,13 @@ impl Display for Status {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         let n = if self.0 & 0b10000000 > 0 { "N" } else { "n" };
         let v = if self.0 & 0b01000000 > 0 { "V" } else { "v" };
+        let u = if self.0 & 0b00100000 > 0 { "U" } else { "u" };
+        let b = if self.0 & 0b00010000 > 0 { "B" } else { "b" };
         let d = if self.0 & 0b00001000 > 0 { "D" } else { "d" };
         let i = if self.0 & 0b00000100 > 0 { "I" } else { "i" };
         let z = if self.0 & 0b00000010 > 0 { "Z" } else { "z" };
         let c = if self.0 & 0b00000001 > 0 { "C" } else { "c" };
-        f.write_str(format!("{}{}{}{}{}{}", n, v, d, i, z, c).as_str())
+        f.write_str(format!("{}{}{}{}{}{}{}{}", n, v, u, b, d, i, z, c).as_str())
     }
 }
 
@@ -143,6 +147,8 @@ struct Cpu {
 }
 
 impl Cpu {
+    const STACK_BASE: u16 = 0x0100;
+
     pub fn new(mem: Rc<RefCell<Memory>>) -> Self {
         Self {
             mem,
@@ -150,7 +156,7 @@ impl Cpu {
             x: 0x00,
             y: 0x00,
             pc: 0xC000,
-            sp: 0x00,
+            sp: 0xFD,
             flags: Status(0b00100100),
             total_cycles: 7,
         }
@@ -164,6 +170,85 @@ impl Cpu {
 
         // Decode
         let (op, addr_mode) = (match &byte {
+            0x48 => Some((PHA, Imp)),
+            0x28 => Some((PLP, Imp)),
+            0xD8 => Some((CLD, Imp)),
+            0xB8 => Some((CLV, Imp)),
+            0x30 => Some((BMI, Rel)),
+            0x29 => Some((AND, Imm)),
+            0x25 => Some((AND, Zpg)),
+            0x35 => Some((AND, ZpgX)),
+            0x2D => Some((AND, Abs)),
+            0x3D => Some((AND, AbsX)),
+            0x39 => Some((AND, AbsY)),
+            0x21 => Some((AND, XInd)),
+            0x31 => Some((AND, IndY)),
+
+            0xA0 => Some((LDY, Imm)),
+            0xA4 => Some((LDY, Zpg)),
+            0xB4 => Some((LDY, ZpgX)),
+            0xAC => Some((LDY, Abs)),
+            0xBC => Some((LDY, AbsX)),
+
+            0xC0 => Some((CPY, Imm)),
+            0xC4 => Some((CPY, Zpg)),
+            0xCC => Some((CPY, Abs)),
+
+            0xE0 => Some((CPX, Imm)),
+            0xE4 => Some((CPX, Zpg)),
+            0xEC => Some((CPX, Abs)),
+
+            0xA2 => Some((LDX, Imm)),
+            0xA6 => Some((LDX, Zpg)),
+            0xB6 => Some((LDX, ZpgY)),
+            0xAE => Some((LDX, Abs)),
+            0xBE => Some((LDX, AbsY)),
+            0x69 => Some((ADC, Imm)),
+            0x65 => Some((ADC, Zpg)),
+            0x75 => Some((ADC, ZpgX)),
+            0x6D => Some((ADC, Abs)),
+            0x7D => Some((ADC, AbsX)),
+            0x79 => Some((ADC, AbsY)),
+            0x61 => Some((ADC, XInd)),
+            0x71 => Some((ADC, IndY)),
+
+            0x49 => Some((EOR, Imm)),
+            0x45 => Some((EOR, Zpg)),
+            0x55 => Some((EOR, ZpgX)),
+            0x4D => Some((EOR, Abs)),
+            0x5D => Some((EOR, AbsX)),
+            0x59 => Some((EOR, AbsY)),
+            0x41 => Some((EOR, XInd)),
+            0x51 => Some((EOR, IndY)),
+
+            0xC9 => Some((CMP, Imm)),
+            0xC5 => Some((CMP, Zpg)),
+            0xD5 => Some((CMP, ZpgX)),
+            0xCD => Some((CMP, Abs)),
+            0xDD => Some((CMP, AbsX)),
+            0xD9 => Some((CMP, AbsY)),
+            0xC1 => Some((CMP, XInd)),
+            0xD1 => Some((CMP, IndY)),
+0xE8 => Some((INX, Imp)),
+0xC8 => Some((INY, Imp)),
+            0xE9 => Some((SBC, Imm)),
+            0xE5 => Some((SBC, Zpg)),
+            0xF5 => Some((SBC, ZpgX)),
+            0xED => Some((SBC, Abs)),
+            0xFD => Some((SBC, AbsX)),
+            0xF9 => Some((SBC, AbsY)),
+            0xE1 => Some((SBC, XInd)),
+            0xF1 => Some((SBC, IndY)),
+
+            0x09 => Some((ORA, Imm)),
+            0x05 => Some((ORA, Zpg)),
+            0x15 => Some((ORA, ZpgX)),
+            0x0D => Some((ORA, Abs)),
+            0x1D => Some((ORA, AbsX)),
+            0x19 => Some((ORA, AbsY)),
+            0x01 => Some((ORA, XInd)),
+            0x11 => Some((ORA, IndY)),
+
             0x68 => Some((PLA, Imp)),
             0x08 => Some((PHP, Imp)),
             0x10 => Some((BPL, Rel)),
@@ -309,8 +394,36 @@ impl Cpu {
         };
 
         match op {
-            ADC => {}
-            AND => {}
+            ADC => {
+                let a = self.a;
+                let ((res, carry), op) = match operand {
+                    None => {
+                        let op = self.mem_read(eff_addr.unwrap());
+                        (op.overflowing_add(self.a.wrapping_add(if self.flags.is_set(Flag::C) { 1 } else { 0 })), op)
+                    }
+                    Some(op) => {
+                        (op.overflowing_add(self.a.wrapping_add(if self.flags.is_set(Flag::C) { 1 } else { 0 })), op)
+                    }
+                };
+                self.a = res;
+
+                self.flags.set(Flag::C, carry);
+                self.flags.set(Flag::V, ((!(a ^ op) & (a ^ res)) & 0x80) > 0);
+                self.flags.set(Flag::Z, self.a == 0x00);
+                self.flags.set(Flag::N, (self.a as i8) < 0);
+            }
+            AND => {
+                match operand {
+                    None => {
+                        self.a &= self.mem_read(eff_addr.unwrap());
+                    }
+                    Some(op) => {
+                        self.a &= op;
+                    }
+                }
+                self.flags.set(Flag::Z, self.a == 0x00);
+                self.flags.set(Flag::N, (self.a as i8) < 0);
+            }
             ASL => {}
             BCC => {
                 if self.flags.is_clear(Flag::C) {
@@ -339,8 +452,8 @@ impl Cpu {
                         op
                     }
                 };
-                self.flags.set(Flag::V, operand & 0x0000_0010 > 0);
-                self.flags.set(Flag::N, operand & 0x0000_0001 > 0);
+                self.flags.set(Flag::N, operand & (Flag::N as u8) > 0);
+                self.flags.set(Flag::V, operand & (Flag::V as u8) > 0);
                 self.flags.set(Flag::Z, operand & self.a == 0);
             }
             BMI => {
@@ -390,21 +503,93 @@ impl Cpu {
                 self.flags.clear(Flag::V);
                 self.inc()
             }
-            CMP => {}
-            CPX => {}
-            CPY => {}
-            DEC => {}
-            DEX => {}
-            DEY => {}
-            EOR => {}
-            INC => {}
-            INX => {
-                self.x.wrapping_add(1);
+            CMP => {
+                let (val, _) = match operand {
+                    None => {
+                        (self.a).overflowing_sub((self.mem_read(eff_addr.unwrap())))
+                    }
+                    Some(op) => {
+                        (self.a).overflowing_sub((op))
+                    }
+                };
+
+                self.flags.set(Flag::C, self.a >= val);
+                self.flags.set(Flag::Z, val == 0x00);
+                self.flags.set(Flag::N, (val as i8) < 0);
+            }
+            CPX => {
+                let (val, _) = match operand {
+                    None => {
+                        (self.x).overflowing_sub((self.mem_read(eff_addr.unwrap())))
+                    }
+                    Some(op) => {
+                        (self.x).overflowing_sub((op))
+                    }
+                };
+
+                self.flags.set(Flag::C, self.x >= val);
+                self.flags.set(Flag::Z, val == 0x00);
+                self.flags.set(Flag::N, (val as i8) < 0);
+            }
+            CPY => {
+                let (val, _) = match operand {
+                    None => {
+                        (self.y).overflowing_sub((self.mem_read(eff_addr.unwrap())))
+                    }
+                    Some(op) => {
+                        (self.y).overflowing_sub((op))
+                    }
+                };
+
+                self.flags.set(Flag::C, self.y >= val);
+                self.flags.set(Flag::Z, val == 0x00);
+                self.flags.set(Flag::N, (val as i8) < 0);
+            }
+            DEC => {
+                self.a = self.a.wrapping_sub(1);
+            }
+            DEX => {
+                self.x = self.x.wrapping_sub(1);
+            }
+            DEY => {
+                self.y = self.y.wrapping_sub(1);
+            }
+            EOR => {
+                match operand {
+                    None => {
+                        self.a ^= self.mem_read(eff_addr.unwrap());
+                    }
+                    Some(op) => {
+                        self.a ^= op;
+                    }
+                }
+                self.flags.set(Flag::Z, self.a == 0x00);
+                self.flags.set(Flag::N, (self.a as i8) < 0);
+            }
+            INC => {
+                let old = self.a;
+                self.a = self.a.wrapping_add(1);
                 self.inc();
+                self.flags.set(Flag::V, ((!(old ^ 1) & (old ^ self.a)) & 0x80) > 0);
+                self.flags.set(Flag::Z, self.a == 0x00);
+                self.flags.set(Flag::N, (self.a as i8) < 0);
+            }
+            INX => {
+                let old = self.x;
+                self.x = self.x.wrapping_add(1);
+                self.inc();
+                self.flags.set(Flag::V, ((!(old ^ 1) & (old ^ self.x)) & 0x80) > 0);
+                self.flags.set(Flag::Z, self.x == 0x00);
+                self.flags.set(Flag::N, (self.x as i8) < 0);
             }
             INY => {
-                self.y.wrapping_add(1);
+                let old = self.y;
+                self.y = self.y.wrapping_add(1);
                 self.inc();
+                self.flags.set(Flag::V, ((!(old ^ 1) & (old ^ self.y)) & 0x80) > 0);
+
+                self.flags.set(Flag::Z, self.y == 0x00);
+                self.flags.set(Flag::N, (self.y as i8) < 0);
             }
             JMP => {
                 self.pc = eff_addr.unwrap();
@@ -454,33 +639,66 @@ impl Cpu {
             }
             LSR => {}
             NOP => self.inc(),
-            ORA => {}
+            ORA => {
+                match operand {
+                    None => {
+                        self.a |= self.mem_read(eff_addr.unwrap());
+                    }
+                    Some(op) => {
+                        self.a |= op;
+                    }
+                }
+                self.flags.set(Flag::Z, self.a == 0x00);
+                self.flags.set(Flag::N, (self.a as i8) < 0);
+            }
             PHA => {
                 self.push(self.a);
+                self.inc();
             }
             PHP => {
-                self.push(self.flags.0);
+                self.push(self.flags.0 | 0b0001_0000);
                 self.inc();
             }
             PLA => {
                 self.a = self.pull();
+                self.flags.set(Flag::Z, self.a == 0x00);
+                self.flags.set(Flag::N, (self.a as i8) < 0);
+                self.inc();
+                self.inc();
             }
             PLP => {
-                self.flags = Status(self.pull());
+                self.flags = Status((self.pull() & 0b1110_1111) | 0b0010_0000);
+                self.inc();
+                self.inc();
             }
             ROL => {}
             ROR => {}
             RTI => {}
             RTS => {
-                let lo = self.pull();
                 let hi = self.pull();
+                let lo = self.pull();
                 self.pc = u16::from_le_bytes([lo, hi]);
                 self.inc();
                 self.inc();
                 self.inc();
-
             }
-            SBC => {}
+            SBC => {
+                let ((val, carry), op) = match operand {
+                    None => {
+                        let op = self.mem_read(eff_addr.unwrap());
+                        ((self.a).overflowing_sub((op)), op)
+                    }
+                    Some(op) => {
+                        ((self.a).overflowing_sub((op)), op)
+                    }
+                };
+                self.flags.set(Flag::V, ((!(self.a ^ op) & (self.a ^ val)) & 0x80) > 0);
+                self.a = val;
+
+                self.flags.set(Flag::C, (self.a >= val) ^ carry);
+                self.flags.set(Flag::Z, val == 0x00);
+                self.flags.set(Flag::N, (val as i8) < 0);
+            }
             SEC => {
                 self.flags.set(Flag::C, true);
                 self.inc()
@@ -544,13 +762,13 @@ impl Cpu {
     }
 
     fn push(&mut self, byte: u8) {
-        self.mem_write(0x0100 + u16::from_le_bytes([self.sp, 0x00]), byte);
-        self.sp += 1;
+        self.mem_write(Cpu::STACK_BASE + u16::from_le_bytes([self.sp, 0x00]), byte);
+        self.sp -= 1;
     }
 
     fn pull(&mut self) -> u8 {
-        let byte  = self.mem_read(0x0100 + u16::from_le_bytes([self.sp, 0x00]));
-        self.sp -= 1;
+        self.sp += 1;
+        let byte = self.mem_read(Cpu::STACK_BASE + u16::from_le_bytes([self.sp, 0x00]));
         byte
     }
 }
@@ -589,6 +807,7 @@ mod test {
         a: u8,
         x: u8,
         y: u8,
+        sp: u8,
         p: Status,
         cyc: usize,
         op: String,
@@ -615,6 +834,7 @@ mod test {
             assert_eq!(cpu.a, want.a, "On step {}, after executing {}, A should be 0x{:02X?}, but was 0x{:02X?}", step, op, want.a, cpu.a);
             assert_eq!(cpu.x, want.x, "On step {}, after executing {}, X should be 0x{:02X?}, but was 0x{:02X?}", step, op, want.x, cpu.x);
             assert_eq!(cpu.y, want.y, "On step {}, after executing {}, Y should be 0x{:02X?}, but was 0x{:02X?}", step, op, want.y, cpu.y);
+            assert_eq!(cpu.sp, want.sp, "On step {}, after executing {}, SP should be 0x{:02X?}, but was 0x{:02X?}", step, op, want.sp, cpu.sp);
             assert_eq!(cpu.flags, want.p, "On step {}, after executing {}, P should be {}, but was {}", step, op, want.p, cpu.flags);
             assert_eq!(cpu.pc, want.pc, "On step {}, after executing {}, PC should be 0x{:04X?}, but was 0x{:04X?}", step, op, want.pc, cpu.pc);
             assert_eq!(cpu.total_cycles, want.cyc, "On step {}, {} should take {} cycles, but took {}", step, op, want.cyc - prev, cpu.total_cycles - prev_cpu);
@@ -641,6 +861,8 @@ mod test {
         let x = u8::from_str_radix(x_as_s, 16).expect("Not a hex");
         let y_as_s = l.get(60..62).unwrap();
         let y = u8::from_str_radix(y_as_s, 16).expect("Not a hex");
+        let sp_as_s = l.get(71..73).unwrap();
+        let sp = u8::from_str_radix(sp_as_s, 16).expect("Not a hex");
         let p_as_s = l.get(65..67).unwrap();
         let p = u8::from_str_radix(p_as_s, 16).expect("Not a hex");
         let cyc_as_s = l.get(90..).unwrap().trim_end();
@@ -652,6 +874,7 @@ mod test {
             a,
             x,
             y,
+            sp,
             p: Status(p),
             cyc,
             op,
